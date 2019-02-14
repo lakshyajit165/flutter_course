b7c53a744519cdc4fce7f28ecc211a3a92b9b7f9
lakshyajit165 <lakshyajit165@gmail.com>
2019-02-15 00:33:21 +0530

    Section 8 complete

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -99,7 +99,6 @@ class _AuthPageState extends State<AuthPage> {
                   height: 20.0,
                 ),
                 RaisedButton(
-                  color: Theme.of(context).primaryColor,
                   textColor: Colors.white,
                   child: Text('LOGIN'),
                   onPressed: _submitForm,
--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -69,7 +69,12 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
+    final double targetPadding = deviceWidth - targetWidth;
+
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
       margin: EdgeInsets.all(10.0),
       child: ListView(
         children: <Widget>[
@@ -79,7 +84,6 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
           SizedBox(height: 10.0,),
           RaisedButton(
             child: Text('Save'),
-            color: Theme.of(context).accentColor,
             textColor: Colors.white,
             onPressed: _submitForm
           )
